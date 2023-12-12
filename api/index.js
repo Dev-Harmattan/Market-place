09f16800b75b098ef2d95df5b27b87422352a967
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-12-12 15:24:54 +0100

    Connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Mongo db is running.');
+  })
+  .catch((err) => console.log(err.message));
 
 const address = app.listen(5000, () => {
   console.log(`Server running on ${address.address().port}`);
