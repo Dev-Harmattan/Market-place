d780bb06e003b2a59f4225b13ad750b68d5637fc
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-12-12 16:55:10 +0100

    create a test route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 
 const app = express();
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -11,6 +15,8 @@ mongoose
     console.log('Mongo db is running.');
   })
   .catch((err) => console.log(err.message));
+
+app.use('/api/user', userRouter)
 
 const address = app.listen(5000, () => {
   console.log(`Server running on ${address.address().port}`);
