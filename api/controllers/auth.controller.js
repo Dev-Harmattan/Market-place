c906dcd34f7fe875f6a13edee2797852caf0b8a1
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-12-12 21:30:32 +0100

    Add sign in route feature

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../util/errorHandler.js';
 
-export const auth = async (req, res, next) => {
+export const signUp = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
@@ -10,6 +12,24 @@ export const auth = async (req, res, next) => {
     return res
       .status(201)
       .json({ message: 'User saved successfully', success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return next(errorHandler(404, 'Wrong Credentials'));
+    const isValidPassword = bcrypt.compareSync(password, user.password);
+    if (!isValidPassword) return next(errorHandler(404, 'Wrong Credentials'));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = user._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
